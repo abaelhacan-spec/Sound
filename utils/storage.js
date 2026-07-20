1accbb9b737be520e3fdf2ee316dc2c738d7e62b
abaelhacan-spec <abaelhacan@gmail.com>
2026-07-20 11:26:40 +0100

    Add files via upload

--- utils/storage.js
+++ utils/storage.js
@@ -4,6 +4,8 @@ const KEYS = {
   FINGERPRINT: 'REFERENCE_FINGERPRINT',
   PHONE_NUMBER: 'TARGET_PHONE_NUMBER',
   THRESHOLD: 'MATCH_THRESHOLD',
+  ALARM_ENABLED: 'ALARM_DETECTION_ENABLED',
+  KNOCK_ENABLED: 'KNOCK_DETECTION_ENABLED',
 };
 
 export async function saveReferenceFingerprint(fingerprint) {
@@ -34,4 +36,19 @@ export async function loadThreshold() {
 
 export async function clearReferenceFingerprint() {
   await AsyncStorage.removeItem(KEYS.FINGERPRINT);
+}
+
+export async function saveDetectionPaths({ alarmEnabled, knockEnabled }) {
+  await AsyncStorage.setItem(KEYS.ALARM_ENABLED, alarmEnabled ? '1' : '0');
+  await AsyncStorage.setItem(KEYS.KNOCK_ENABLED, knockEnabled ? '1' : '0');
+}
+
+export async function loadDetectionPaths() {
+  const alarm = await AsyncStorage.getItem(KEYS.ALARM_ENABLED);
+  const knock = await AsyncStorage.getItem(KEYS.KNOCK_ENABLED);
+  return {
+    // القيم الافتراضية: المسارين مفعّلين (نفس السلوك السابق) لو لسه محدّش اختار
+    alarmEnabled: alarm === null ? true : alarm === '1',
+    knockEnabled: knock === null ? true : knock === '1',
+  };
 }
